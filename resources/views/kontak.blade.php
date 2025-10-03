39912c8886a6724b1a8328cdee6bf53972f04196
Revalina <revalina4523091@univpancasila.ac.id>
2025-10-03 11:14:52 +0700

    latihan laravel

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Kontak Kami - LaraPress</title>
+</head>
+
+<body>
+    <h1>Kontak LaraPress</h1>
+    <p>Nama : Revalina Adelia</p>
+    <p>NPM  : 4523210091</p>
+    <p>Mata Kuliah : Prak. Pemrograman Berbasis Web (A)</p>
+    <p>Email : revalina4523091@univpancasila.ac.id</p>
+    <a href="/tentang-kami">Lihat Halaman Tentang Kami</a>
+    <br>
+    <a href="/">Kembali ke Halaman Utama</a>
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Selamat Datang di LaraPress</title>
+</head>
+<body>
+    <h1>Selamat Datang di Blog LaraPress</h1>
+    <p>Ini adalah halaman utama dari aplikasi blog kita.</p>
+    <a href="/tentang-kami">Lihat Halaman Tentang Kami</a>
+    <br>
+    <a href="/kontak">Lihat Halaman Kontak</a>
+</body>
+</html>
+
